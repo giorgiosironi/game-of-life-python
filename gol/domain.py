a581d9586c251b9a53929fd026dae1af79b2186c
Giorgio Sironi <info@giorgiosironi.com>
2016-04-19 11:02:59 +0200

    Implemented Cell.neighbors()

--- gol/domain.py
+++ gol/domain.py
@@ -1,2 +1,39 @@
 def answer():
     return(42)
+
+class Cell:
+    def __init__(self, x, y):
+        # TODO: idiomatic usage of fields and their scope
+        self._x = x
+        self._y = y
+
+    def neighbors(self):
+        # TODO: use list comprehension
+        return frozenset([
+            Cell(self._x-1, self._y-1),
+            Cell(self._x-1, self._y),
+            Cell(self._x-1, self._y+1),
+            Cell(self._x, self._y-1),
+            Cell(self._x, self._y+1),
+            Cell(self._x+1, self._y-1),
+            Cell(self._x+1, self._y),
+            Cell(self._x+1, self._y+1),
+        ])
+
+    def __eq__(self, another):
+        return(self._x == another._x and self._y == another._y)
+
+    def __hash__(self):
+        return(self._x * 31 + self._y)
+
+    def __str__(self):
+        return("Cell(%d, %d)" % (self._x, self._y))
+
+    def __repr__(self):
+        return(self.__str__())
+
+    def x(self):
+        return(self._x)
+
+    def y(self):
+        return(self._y)
--- tests/test_domain.py
+++ tests/test_domain.py
@@ -1,4 +1,15 @@
 from gol import domain
+from gol.domain import Cell
 
 def test_true():
     assert 42 == domain.answer()
+
+def test_cell_neighbors():
+    cell = Cell(0, 0)
+    neighbors = cell.neighbors()
+    assert 8 == len(neighbors)
+    for n in neighbors:
+        assert abs(n.x()) <= 1
+        assert abs(n.y()) <= 1
+        assert not n == cell
+
