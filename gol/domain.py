daabbcc68143a3ededcc6bfb4a50523469c09977
Giorgio Sironi <info@giorgiosironi.com>
2016-04-20 09:36:30 +0200

    Idiom: return does not need parentheses

--- gol/domain.py
+++ gol/domain.py
@@ -21,22 +21,22 @@ class Cell:
         return self.neighbors() | frozenset([self])
 
     def __eq__(self, another):
-        return(self._x == another._x and self._y == another._y)
+        return self._x == another._x and self._y == another._y
 
     def __hash__(self):
-        return(self._x * 31 + self._y)
+        return self._x * 31 + self._y
 
     def __str__(self):
-        return("Cell({}, {})".format(self._x, self._y))
+        return "Cell({}, {})".format(self._x, self._y)
 
     def __repr__(self):
-        return(self.__str__())
+        return self.__str__()
 
     def x(self):
-        return(self._x)
+        return self._x
 
     def y(self):
-        return(self._y)
+        return self._y
 
 class State:
     alive = True
@@ -46,26 +46,26 @@ class ClassicRules:
     def next_state(self, state, alive_neighbors):
         if (state == State.alive):
             if (alive_neighbors >= 2 and alive_neighbors <= 3):
-                return(State.alive)
+                return State.alive
         else:
             if (alive_neighbors == 3):
-                return(State.alive)
-        return(State.dead)
+                return State.alive
+        return State.dead
 
 class Generation:
     @staticmethod
     def horizontal_bar():
-        return(Generation(frozenset([Cell(-1, 0), Cell(0, 0), Cell(1, 0)])))
+        return Generation(frozenset([Cell(-1, 0), Cell(0, 0), Cell(1, 0)]))
     @staticmethod
     def vertical_bar():
-        return(Generation(frozenset([Cell(0, -1), Cell(0, 0), Cell(0, 1)])))
+        return Generation(frozenset([Cell(0, -1), Cell(0, 0), Cell(0, 1)]))
     @staticmethod
     def block():
-        return(Generation(frozenset([Cell(0, 0), Cell(0, 1), Cell(1, 0), Cell(1, 1)])))
+        return Generation(frozenset([Cell(0, 0), Cell(0, 1), Cell(1, 0), Cell(1, 1)]))
     def __init__(self, alive_cells):
         self._alive_cells = alive_cells
     def __eq__(self, another):
-        return(self._alive_cells == another._alive_cells)
+        return self._alive_cells == another._alive_cells
     def __repr__(self):
         return "Generation({})".format(self._alive_cells)
     def evolve(self, rules):
@@ -77,8 +77,8 @@ class Generation:
         return Generation(frozenset(alive))
     def _state(self, cell):
         if cell in self._alive_cells:
-            return(State.alive)
+            return State.alive
         else:
-            return(State.dead)
+            return State.dead
     def _alive_neighbors(self, cell):
-        return(len({n for n in cell.neighbors() if n in self._alive_cells}))
+        return len({n for n in cell.neighbors() if n in self._alive_cells})
